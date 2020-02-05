a6ffd1b94e116879bf32e4246bc6bbccf6564ed4
Nathalia <natylafosse@gmail.com>
2020-02-04 19:20:06 -0500

    timesup

--- Program.cs
+++ Program.cs
@@ -4,9 +4,41 @@ namespace livecodingfinal
 {
     class Program
     {
+        public static double Candy(string weather, int temp)
+        {
+            double avgVisitor = 250.0;
+
+            // first determine the kind of weather
+            //if "Clear" - 10% more
+            //if "Cloudy" -  0%
+            //if "Raining" - 25% less
+            //if "Full Moon" - 25% more
+            if (weather == "Clear")
+            {
+                avgVisitor = (avgVisitor *.10) + avgVisitor;
+            }
+
+            else if (weather == "Cloudy")
+            {
+                avgVisitor = avgVisitor;
+            }
+
+            else if (weather == "Raining")
+            {
+                avgVisitor = avgVisitor - (avgVisitor * .45);
+            }
+            // then the temperature
+
+            return avgVisitor * 3;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Candy("Clear", 50));
+            Console.WriteLine(Candy("Full Moon", 40));
+            Console.WriteLine(Candy("Raining", 70));
         }
+
+
     }
 }
