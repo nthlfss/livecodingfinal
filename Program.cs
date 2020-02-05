0441e8886e465f13153b2c89887deddbb6a8e8ed
Nathalia <natylafosse@gmail.com>
2020-02-05 18:13:46 -0500

    went back to complete it

--- Program.cs
+++ Program.cs
@@ -4,10 +4,35 @@ namespace livecodingfinal
 {
     class Program
     {
-        public static double Candy(string weather, int temp)
-        {
-            double avgVisitor = 250.0;
+        static double avgVisitor = 250;
+        static double avgTemp;
+        static double avgWeather;
 
+
+        static double Temp(string temp)
+        {
+            if (temp == "40s")
+            {
+                avgTemp = avgVisitor * -.05;
+            }
+            else if (temp == "50s")
+            {
+                return avgTemp;
+            }
+            else if (temp == "60s")
+            {
+                avgTemp = avgVisitor * .05;
+            }
+            else if (temp == "70s")
+            {
+                avgTemp = avgVisitor * .2;
+            }
+
+            return avgTemp;
+        }
+
+        static double Weather(string weather)
+        {
             // first determine the kind of weather
             //if "Clear" - 10% more
             //if "Cloudy" -  0%
@@ -15,28 +40,39 @@ namespace livecodingfinal
             //if "Full Moon" - 25% more
             if (weather == "Clear")
             {
-                avgVisitor = (avgVisitor *.10) + avgVisitor;
+                avgWeather = avgVisitor *.10;
             }
 
             else if (weather == "Cloudy")
             {
-                avgVisitor = avgVisitor;
+                return avgWeather;
             }
 
             else if (weather == "Raining")
             {
-                avgVisitor = avgVisitor - (avgVisitor * .45);
+                avgWeather = avgVisitor * -.25;
             }
-            // then the temperature
 
-            return avgVisitor * 3;
+            else if (weather == "Full Moon")
+            {
+                avgWeather = avgVisitor * .25;
+            }
+
+            return avgWeather;
+        }
+
+        static double Candy(string weather, string temp)
+        {
+            // 3 candy per kid
+            double avgCandy = (Weather(weather) + Temp(temp) + avgVisitor) * 3;
+            return avgCandy;
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine(Candy("Clear", 50));
-            Console.WriteLine(Candy("Full Moon", 40));
-            Console.WriteLine(Candy("Raining", 70));
+            Console.WriteLine(Candy("Clear", "50s"));
+            Console.WriteLine(Candy("Full Moon","40s"));
+            Console.WriteLine(Candy("Raining", "70s"));
         }
 
 
